35a4a4359083e936b59c71aa3882233dd496f100
Никита <click.95@mail.ru>
2018-01-01 23:23:31 +0700

    fix

--- GelbooruChannelBot/GelbooruPost.cs
+++ GelbooruChannelBot/GelbooruPost.cs
@@ -103,7 +103,7 @@ namespace GelbooruChannelBot
 
         public string GetPostLink()
         {
-            return @"https://gelbooru.com/index.php?page=post&s=view&id={post.Id}";
+            return @"https://gelbooru.com/index.php?page=post&s=view&id={Id}";
         }
 
         public string GetId()
